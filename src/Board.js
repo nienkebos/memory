c03b9a8071eeace6a82a98fc630a967954716d75
nienkebos <nienkebos@live.com>
2016-08-30 23:14:31 +0200

    Cannot pick the same card twice in one turn

--- src/Board.js
+++ src/Board.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import Card from './Card'
 
-
 class Board extends React.Component {
-
   constructor (){
     super()
 
@@ -16,6 +14,8 @@ class Board extends React.Component {
       ],
       turn: [],
     }
+
+    this.previousCard
   }
 
   emptyTurns() {
@@ -28,18 +28,28 @@ class Board extends React.Component {
     if (turn[0] === turn[1]) {
       console.log("We have a winning pair")
     } else {
-    console.log("Sorry, no pair")
-  };
+      console.log("Sorry, no pair")
+    };
     this.emptyTurns();
   }
 
-  onPickCard(pair) {
-    this.state.turn.push(pair)
-    console.log( this.state.turn )
+  onPickCard(props) {
+    if (this.state.turn.length == 0) {
+      this.state.turn.push( props.pair )
+      console.log("1st turn", this.state.turn )
+      }
+    else if (this.state.turn.length == 1 && this.previousCard !== props.cardId) {
+      this.state.turn.push( props.pair )
+      console.log("2nd turn", this.state.turn )
+      }
+    else {
+      console.log("Error, you can not pick the same card!")
+    }
 
     if (this.state.turn.length == 2) {
       this.compareCards(this.state.turn)
     }
+    this.previousCard = props.cardId
   }
 
   renderCard(card, index) {
--- src/Card.js
+++ src/Card.js
@@ -1,20 +1,15 @@
 import React from 'react'
 
 class Card extends React.Component {
-
-
   pickFirstCard() {
-    this.props.onPick(this.props.pair)
+    this.props.onPick(this.props)
   }
 
   render () {
-
     return (
-
-      <div onClick={this.pickFirstCard.bind(this)} style={{ width: '100px', height: '100px', 'backgroundColor': '#ccc', float: 'left', margin: "4px"}} >
-      {this.props.cardId}
+      <div onClick={this.pickFirstCard.bind(this)} style={{ width: '100px', height: '100px', 'backgroundColor': '#ccc', float: 'left', margin: "4px"}}>
+        {this.props.cardId}
       </div>
-
     );
   }
 }
