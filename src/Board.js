974278e99432b99f890f5ff415d575f8e5ff7222
Mark Nuiver <mark@vrijblikken.nl>
2016-08-30 17:01:38 +0200

    FirstClick Halfwa.....

--- src/Board.js
+++ src/Board.js
@@ -13,14 +13,35 @@ class Board extends React.Component {
         {id: 2, pair: 1, visible: false, completed: true },
         {id: 3, pair: 2, visible: false, completed: true },
         {id: 4, pair: 2, visible: false, completed: true },
-      ]
+      ],
+      turn: [],
     }
   }
 
-  renderCard(card, index) {
-    return <Card
-    key={ index }
-    id={ card.id }
+  addIdToTurn(card) {
+    let newCards = cards.map((eachcard) => {
+      if (eachcard.id == card.id) {
+        return {
+          id: card.id,
+          pair: pair
+          visible: 
+        }
+      }
+      return player;
+    })
+  }
+
+  firstClick(card) {
+    console.log(card.id)
+
+  }
+
+  renderCard(card) {
+    return (
+      <div onClick={this.firstClick.bind(card)} style={{ width: '100px', height: '100px', 'background-color': '#ccc', float: 'left', margin: "4px"}} >
+      {card.id}
+      </div>
+    )
   }
 
 
@@ -30,7 +51,7 @@ class Board extends React.Component {
       <div>
       <h1>Memory Game</h1>
 
-        <div>
+        <div style = {{ width: '216px' }}>
           {this.state.cards.map(this.renderCard.bind(this))}
         </div>
 
